beb4331eb4f34b85b7d6a7285ee706f811e77f30
Arun <sarun6153@gmail.com>
2021-07-12 19:43:12 +0530

    Removing the Keys of AWS.

--- env/enviornment_variables.js
+++ env/enviornment_variables.js
@@ -2,11 +2,8 @@ module.exports =
 { 
     port : 3000,
     aws_SES_Credentials : {
-        // username:"sarun6153@gmail.com",
-        // password:"%N5&F=sM(g+V&km",
-        // consoleLoginLink:"https://916662533887.signin.aws.amazon.com/console",
-        accessKeyId:"AKIA5K3KYH37TQEHAHWN",
-        secretAccessKey:"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",
+        accessKeyId:"",
+        secretAccessKey:"",
         region:"",
         SenderEmailId:""
     }
